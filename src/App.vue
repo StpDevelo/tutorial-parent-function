8a85b11e0f48b8dab24a66ed1a2080807bd2db23
Stp.Develo <stp.develo@gmail.com>
2017-05-26 12:21:28 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    handlerParent() {
+      alert(`It's Me Parent`)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="title" @click="handler">
+    Hello Home
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handler() {
+      this.$parent.handlerParent()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  margin-top: 10%;
+  font-size: 34px;
+  text-align: center;
+}
+.title:hover {
+  cursor: pointer;
+  color: rebeccapurple;
+}
+</style>
+
